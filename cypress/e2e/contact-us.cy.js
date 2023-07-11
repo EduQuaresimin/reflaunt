aa6125c5605a798e30c2200be4783d195b66bae4
Eduardo Quaresimin Santos <eduardoquaresimin@gmail.com>
2023-07-11 12:01:49 +0100

    Contact-us page covered
    Positive and negative functional scenarios of the "Contact-us" page covered with automated tests.

---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker'
+
+describe('Contact-us', () => {
+  beforeEach(() => {
+    cy.visit('https://www.reflaunt.com/contact-us')
+  })
+  context('Positive functional scenarios', () => {
+    it('Validate the submission of the "contact-us" form by filling in all fields correctly', () => {
+      cy.fillContactUsForm()
+
+      cy.contains('button', 'Submit').should('be.visible')
+    })
+  })
+  context('Negative functional scenarios', () => {
+    it('Validate the submission of the "contact-us" form without filling in the "Name" field', () => {
+      cy.get('#email').type(faker.internet.email())
+      cy.get('#organisation').type(faker.lorem.word())
+      cy.get('#message').type(faker.lorem.words(10))
+      cy.contains('button', 'Submit').click()
+
+      cy.get('#name').should('have.css', 'border-color', 'rgb(218, 7, 20)')
+    })   
+    it('Validate the submission of the "contact-us" form without using the character "@" at "Email" field', () => {
+      cy.get('#name').type(faker.lorem.word())
+      cy.get('#email').type(faker.lorem.word())
+      cy.get('#organisation').type(faker.lorem.word())
+      cy.get('#message').type(faker.lorem.words(10))
+      cy.contains('button', 'Submit').click()
+    })
+    it('Validate the submission of the "contact-us" form without filling in the "Organisation" field', () => {
+      cy.get('#name').type(faker.lorem.words(2))
+      cy.get('#email').type(faker.internet.email())
+      cy.get('#message').type(faker.lorem.words(10))      
+      cy.contains('button', 'Submit').click()
+
+      cy.get('#organisation').should('have.css', 'border-color', 'rgb(218, 7, 20)')
+    })
+    it('Validate the submission of the "contact-us" form without filling in the "Message" field', () => {
+      cy.get('#name').type(faker.lorem.words(2))
+      cy.get('#email').type(faker.internet.email())
+      cy.get('#organisation').type(faker.lorem.word())      
+      cy.contains('button', 'Submit').click()
+
+      cy.get('#message').should('have.css', 'border-color', 'rgb(218, 7, 20)')
+    })
+  })
+})
