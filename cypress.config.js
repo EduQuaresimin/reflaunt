d145bf2b2bb06d11141d9908c362c2cfd3726c58
Eduardo Quaresimin Santos <eduardoquaresimin@gmail.com>
2023-07-11 18:48:00 +0100

    Add ci pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'https://jsonplaceholder.typicode.com',
     viewportHeight: 768,
     viewportWidth: 1366,
+    fixturesFolder: false,
     setupNodeEvents(on, config) {
     }
   },
